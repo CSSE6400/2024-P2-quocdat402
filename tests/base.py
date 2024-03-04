678bb972ae64b61e767d54454a901553998411d0
quocdat402 <43123171+quocdat402@users.noreply.github.com>
2024-03-04 03:53:01 +0000

    Fix failed test

--- tests/base.py
+++ tests/base.py
@@ -8,6 +8,7 @@ class TodoTest(unittest.TestCase):
             'TESTING': True
         })
         self.client = self.app.test_client()
+        
     def assertDictSubset(self, expected_subset: dict, whole: dict):
         for key, value in expected_subset.items():
             self.assertEqual(whole[key], value)
--- todo/__init__.py
+++ todo/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 def create_app(config_overrides=None):
     app = Flask(__name__)
 
@@ -11,11 +12,14 @@ def create_app(config_overrides=None):
     from todo.models import db
     from todo.models.todo import Todo
     db.init_app(app)
+
     # Create the database tables
     with app.app_context():
         db.create_all()
         db.session.commit()
+
     # Register the blueprints
     from .views.routes import api
     app.register_blueprint(api)
+    
     return app
